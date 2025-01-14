65278c790503ad230eb9836d134b3352d9251422
Xar0n <dima.dima.maksimov1998@mail.ru>
2025-01-14 21:17:10 +0300

    Create Basket microservice

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,7 +1,51 @@
+using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(assembly);
+    cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
+
+var connectionString = builder.Configuration.GetConnectionString("Database")!;
+builder.Services.AddMarten(o =>
+{
+    o.Connection(connectionString);
+    o.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+var connectionStringRedis = builder.Configuration.GetConnectionString("Redis")!;
+builder.Services.AddStackExchangeRedisCache(o =>
+{
+    o.Configuration = connectionStringRedis;
+});
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(connectionString)
+    .AddRedis(connectionStringRedis);
 
 var app = builder.Build();
 
+app.MapCarter();
+
+app.UseExceptionHandler(o => { });
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
